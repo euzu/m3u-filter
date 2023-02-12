d6ad287ceda311ee7a10abbc7f969b5cbd921aa6
euzu <euzu@github.com>
2023-02-12 01:36:21 +0100

    Refactored mapping, breaking changes for mapping configuration.

--- src/mapping.rs
+++ src/mapping.rs
@@ -1,86 +1,39 @@
+use std::collections::HashMap;
+use crate::filter::{Filter, get_filter, PatternTemplate, RegexWithCaptures, ValueProcessor};
+use crate::m3u::PlaylistItem;
+use crate::model::ItemField;
+
 fn default_as_false() -> bool { false }
 fn default_as_empty_str() -> String { String::from("") }
-
-#[derive(Debug)]
-pub struct MapperRe {
-    pub re: regex::Regex,
-    pub captures: Vec<String>,
-}
-
-impl Clone for MapperRe {
-    fn clone(&self) -> Self {
-        MapperRe {
-            re: self.re.clone(),
-            captures: self.captures.clone()
-        }
-    }
-}
+fn default_as_empty_map() -> HashMap<String, String> { HashMap::new() }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Mapper {
-    pub tvg_name: String,
-    pub tvg_names: Vec<String>,
-    pub tvg_id: String,
-    pub tvg_chno: String,
-    pub tvg_logo: String,
-    pub group_title: Vec<String>,
+    pub pattern: String,
+    #[serde(default = "default_as_empty_map")]
+    attributes: HashMap<String, String>,
+    #[serde(default = "default_as_empty_map")]
+    suffix: HashMap<String, String>,
+    #[serde(default = "default_as_empty_map")]
+    prefix: HashMap<String, String>,
     #[serde(skip_serializing, skip_deserializing)]
-    pub _re: Option<Vec<MapperRe>>,
+    pub _filter: Option<Filter>,
 }
 
 impl Mapper {
-    pub(crate) fn prepare<'a>(&mut self, templates: Option<&Vec<MapperTemplate>>) -> () {
-        let mut rev = Vec::new();
-        for rs in &self.tvg_names {
-            let mut regstr = String::from(rs);
-            let empty_vec = Vec::new();
-            let templ : &Vec<MapperTemplate> = templates.unwrap_or(&empty_vec);
-            for t in templ {
-                regstr = regstr.replace(format!("!{}!", &t.name).as_str(), &t.value);
-            }
-
-            let re = regex::Regex::new(regstr.as_str());
-            if re.is_err() {
-                println!("cant parse regex: {}", &regstr);
-                std::process::exit(1);
-            } else {
-                let regexp = re.unwrap();
-                let captures = regexp.capture_names().map(|x| String::from(x.unwrap_or(""))).filter(|x| x.len() > 0).collect::<Vec<String>>();
-                rev.push(MapperRe {
-                    re: regexp,
-                    captures
-                })
-            }
-        }
-        self._re = Some(rev);
+    pub(crate) fn prepare<'a>(&mut self, templates: Option<&Vec<PatternTemplate>>) -> () {
+        self._filter = Some(get_filter(&self.pattern, templates));
     }
 }
 
 impl Clone for Mapper {
     fn clone(&self) -> Self {
         Mapper {
-            tvg_name: self.tvg_name.clone(),
-            tvg_names: self.tvg_names.clone(),
-            tvg_id: self.tvg_id.clone(),
-            tvg_chno: self.tvg_chno.clone(),
-            tvg_logo: self.tvg_logo.clone(),
-            group_title: self.group_title.clone(),
-            _re: self._re.clone(),
-        }
-    }
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct MapperTemplate {
-    pub name: String,
-    pub value: String,
-}
-
-impl Clone for MapperTemplate {
-    fn clone(&self) -> Self {
-        MapperTemplate {
-            name: self.name.clone(),
-            value: self.value.clone()
+            pattern: self.pattern.clone(),
+            attributes: self.attributes.clone(),
+            suffix: self.suffix.clone(),
+            prefix: self.prefix.clone(),
+            _filter: self._filter.clone(),
         }
     }
 }
@@ -107,13 +60,120 @@ impl Clone for MappingTag {
     }
 }
 
+pub struct MappingValueProcessor<'a> {
+    pub(crate) pli: &'a mut PlaylistItem,
+    pub(crate) mapping_tag: &'a MappingTag,
+    pub(crate) mapper: &'a Mapper,
+}
+
+impl ValueProcessor for MappingValueProcessor<'_> {
+    fn process<'a>(&mut self, _: &ItemField, value: &str, rewc: &RegexWithCaptures, verbose: bool) -> bool {
+        let mut tag = "".to_string();
+        //let mut captured_values = HashMap::new();
+        if rewc.captures.len() > 0 {
+            let captures_opt = rewc.re.captures(value);
+            if captures_opt.is_some() {
+                let captures = captures_opt.unwrap();
+                for cname in &rewc.captures {
+                    let match_opt = captures.name(cname.as_str());
+                    let repl = if match_opt.is_some() {
+                        match_opt.map_or("", |m| m.as_str())
+                    } else {
+                        ""
+                    };
+                    //captured_values.insert(cname, repl);
+                    for c in &self.mapping_tag.captures {
+                        if c.eq(cname) {
+                            if tag.is_empty() {
+                                tag = String::from(repl);
+                            } else {
+                                tag = format!("{}{}{}", tag,  &self.mapping_tag.concat, String::from(repl))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (key, value) in &self.mapper.attributes {
+            let new_value = String::from(value);
+            if verbose { println!("Attribute {} set to {}", key, new_value)}
+            if "name".eq(key) {
+                self.pli.header.name = String::from(new_value);
+            } else if "group".eq(key) {
+                self.pli.header.group = String::from(new_value);
+            } else if "title".eq(key) {
+                self.pli.header.title = String::from(new_value);
+            } else if "logo".eq(key) {
+                self.pli.header.logo = String::from(new_value);
+            } else if "id".eq(key) {
+                self.pli.header.id = String::from(new_value);
+            } else if "chno".eq(key) {
+                self.pli.header.chno = String::from(new_value);
+            } else {
+                println!("Unknown field {} in attributes", new_value);
+            }
+        }
+
+        if !tag.is_empty() {
+            tag = format!("{}{}{}", &self.mapping_tag.prefix, tag, &self.mapping_tag.suffix)
+        }
+
+        for (key, value) in &self.mapper.suffix {
+            let mut new_value = String::from(value);
+            if value.contains("<tag>") {
+                if tag.is_empty() {
+                    if verbose { println!("No tag exists, skipping tag suffix for {}", key) }
+                    continue;
+                }
+                new_value = String::from(value.replace("<tag>", &tag));
+            }
+
+            if verbose { println!("Adding suffix to {}: {}", key, new_value)}
+            if "name".eq(key) {
+                self.pli.header.name = format!("{}{}", &self.pli.header.name,  new_value).to_string();
+            } else if "group".eq(key) {
+                self.pli.header.group = format!("{}{}", &self.pli.header.group,  new_value).to_string();
+            } else if "title".eq(key) {
+                self.pli.header.title = format!("{}{}", &self.pli.header.title,  new_value).to_string();
+            } else {
+                println!("Unknown field {} in suffix", key);
+            }
+        }
+
+        for (key, value) in &self.mapper.prefix {
+            let mut new_value = String::from(value);
+            if value.contains("<tag>") {
+                if tag.is_empty() {
+                    if verbose { println!("No tag exists, skipping tag prefix for {}", key) }
+                    continue;
+                }
+                new_value = String::from(value.replace("<tag>", &tag));
+            }
+
+            if verbose { println!("Adding prefix to {}: {}", key, new_value)}
+            if "name".eq(key) {
+                self.pli.header.name = format!("{}{}", new_value, &self.pli.header.name).to_string();
+            } else if "group".eq(key) {
+                self.pli.header.group = format!("{}{}", new_value, &self.pli.header.group).to_string();
+            } else if "title".eq(key) {
+                self.pli.header.title = format!("{}{}",  new_value, &self.pli.header.title).to_string();
+            } else {
+                println!("Unknown field {} in prefix", key);
+            }
+        }
+        true
+    }
+}
+
+
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Mapping {
     pub id: String,
     pub tag: Option<MappingTag>,
     #[serde(default = "default_as_false")]
     pub match_as_ascii: bool,
-    pub templates: Option<Vec<MapperTemplate>>,
+    pub templates: Option<Vec<PatternTemplate>>,
     pub mapper: Vec<Mapper>,
 }
 
