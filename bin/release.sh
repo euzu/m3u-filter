44e2036a42af4e71d1bb56e43d7458dc633b5fc0
euzu <euzu@github.com>
2023-12-06 15:04:05 +0100

    new relase 1.1.4

--- bin/release.sh
+++ bin/release.sh
@@ -26,7 +26,7 @@ DARWIN_ARC=${DARWIN_DIR}.tgz
 RASPI_ARC=${RASPI_DIR}.tgz
 
 ./bin/build_lin_static.sh && \
-./bin/build_darwin.sh && \
+# ./bin/build_darwin.sh && \
 ./bin/build_raspi.sh && \
 ./bin/build_win.sh && \
 ./bin/build_fe.sh && \
@@ -35,34 +35,35 @@ rm -rf "$LIN_DIR" "$RASPI_DIR" "$DARWIN_DIR" "$WIN_DIR" "$LIN_ARC" "$RASPI_ARC" 
 mkdir "$LIN_DIR" && \
 mkdir "$WIN_DIR" && \
 mkdir "$RASPI_DIR" && \
-mkdir "$DARWIN_DIR" && \
+# mkdir "$DARWIN_DIR" && \
 cp x86_64-unknown-linux-musl/release/m3u-filter "$LIN_DIR" && \
-cp x86_64-apple-darwin/release/m3u-filter "$DARWIN_DIR" && \
+# cp x86_64-apple-darwin/release/m3u-filter "$DARWIN_DIR" && \
 cp armv7-unknown-linux-musleabihf/release/m3u-filter "$RASPI_DIR" && \
 cp x86_64-pc-windows-gnu/release/m3u-filter.exe "$WIN_DIR" && \
 cp ../*.yml "$LIN_DIR" && \
 cp ../*.yml "$WIN_DIR" && \
-cp ../*.yml "$DARWIN_DIR" && \
+# cp ../*.yml "$DARWIN_DIR" && \
 cp ../*.yml "$RASPI_DIR" && \
 cp -rf ../frontend/build "$LIN_DIR"/web && \
 cp -rf ../frontend/build "$WIN_DIR"/web && \
-cp -rf ../frontend/build "$DARWIN_DIR"/web && \
+# cp -rf ../frontend/build "$DARWIN_DIR"/web && \
 cp -rf ../frontend/build "$RASPI_DIR"/web && \
 tar cvzf "$LIN_ARC" "$LIN_DIR" && \
-tar cvzf "$DARWIN_ARC" "$DARWIN_DIR" && \
+#  tar cvzf "$DARWIN_ARC" "$DARWIN_DIR" && \
 tar cvzf "$RASPI_ARC" "$RASPI_DIR" && \
 zip -r "$WIN_ARC" "$WIN_DIR" && \
 shasum -a 256 "$LIN_ARC" > checksum.txt && \
-shasum -a 256 "$DARWIN_ARC" >> checksum.txt && \
+# shasum -a 256 "$DARWIN_ARC" >> checksum.txt && \
 shasum -a 256 "$RASPI_ARC" >> checksum.txt && \
 shasum -a 256 "$WIN_ARC" >> checksum.txt && \
 mkdir "release_${VERSION}" && \
 mv "$LIN_ARC" "release_${VERSION}" && \
-mv "$DARWIN_ARC" "release_${VERSION}" && \
+# mv "$DARWIN_ARC" "release_${VERSION}" && \
 mv "$RASPI_ARC" "release_${VERSION}" && \
 mv "$WIN_ARC"  "release_${VERSION}" && \
 mv checksum.txt "release_${VERSION}" && \
-rm -rf "$LIN_DIR" "$DARWIN_DIR" "$RASPI_DIR" "$WIN_DIR" && \
+# rm -rf "$LIN_DIR" "$DARWIN_DIR" "$RASPI_DIR" "$WIN_DIR" && \
+rm -rf "$LIN_DIR" "$RASPI_DIR" "$WIN_DIR" && \
 git add . && \
 git commit -m "release ${VERSION}" && \
 git tag -a "$VERSION" -m "$VERSION" && \
