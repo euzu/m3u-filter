865a5d1f71051376346981b1ec19c0a1ab6766bc
euzu <euzu@github.com>
2024-03-06 14:16:59 +0100

    added build and deploy docker script

---
+++ bin/build_github_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source "${HOME}"/.ghcr.io
+cd ./docker && cp ../target/x86_64-unknown-linux-musl/release/m3u-filter . && rm -rf ./web && cp -r ../frontend/build ./web
+
+if [ $? -eq 0 ]; then
+  VERSION=$(./m3u-filter -V | sed 's/m3u-filter *//')
+  if [ -n "${VERSION}" ]; then
+    echo "building docker image version ${VERSION}"
+    docker build -t ghcr.io/euzu/m3u-filter:"${VERSION}" . && \
+    docker ghcr.io/euzu/m3u-filter:"${VERSION}" ghcr.io/euzu/m3u-filter:latest  && \
+    docker login ghcr.io -u euzu -p "${GHCR_IO_TOKEN}" && \
+    docker push ghcr.io/euzu/m3u-filter:"${VERSION}" && \
+    docker push ghcr.io/euzu/m3u-filter:latest && \
+    rm -rf ./web && \
+    rm -f ./m3u-filter
+  fi
+fi
