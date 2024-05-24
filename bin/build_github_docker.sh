f0092e27c728103c30342ffe445cd5c040223f8f
euzu <euzu@github.com>
2024-05-24 16:42:03 +0200

    refactored shell scripts

--- bin/build_github_docker.sh
+++ bin/build_github_docker.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-source "${HOME}"/.ghcr.io
-cd ./docker && cp ../target/x86_64-unknown-linux-musl/release/m3u-filter . && rm -rf ./web && cp -r ../frontend/build ./web
+set -e
+set -o pipefail
 
-if [ $? -eq 0 ]; then
-  VERSION=$(./m3u-filter -V | sed 's/m3u-filter *//')
-  if [ -n "${VERSION}" ]; then
-    echo "building docker image version ${VERSION}"
-    docker build -f Dockerfile-manual -t ghcr.io/euzu/m3u-filter:"${VERSION}" . && \
-    docker tag ghcr.io/euzu/m3u-filter:"${VERSION}" ghcr.io/euzu/m3u-filter:latest  && \
-    docker login ghcr.io -u euzu -p "${GHCR_IO_TOKEN}" && \
-    docker push ghcr.io/euzu/m3u-filter:"${VERSION}" && \
-    docker push ghcr.io/euzu/m3u-filter:latest && \
-    rm -rf ./web && \
-    rm -f ./m3u-filter
-  fi
+source "${HOME}/.ghcr.io"
+
+cd ./docker
+cp ../target/x86_64-unknown-linux-musl/release/m3u-filter .
+rm -rf ./web
+cp -r ../frontend/build ./web
+
+VERSION=$(./m3u-filter -V | sed 's/m3u-filter *//')
+if [ -n "${VERSION}" ]; then
+  echo "Building Docker image version ${VERSION}"
+  
+  docker build -f Dockerfile-manual -t ghcr.io/euzu/m3u-filter:"${VERSION}" .
+  docker tag ghcr.io/euzu/m3u-filter:"${VERSION}" ghcr.io/euzu/m3u-filter:latest
+  
+  docker login ghcr.io -u euzu -p "${GHCR_IO_TOKEN}"
+  
+  docker push ghcr.io/euzu/m3u-filter:"${VERSION}"
+  docker push ghcr.io/euzu/m3u-filter:latest
+  
+  rm -rf ./web
+  rm -f ./m3u-filter
 fi
--- bin/release.sh
+++ bin/release.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+set -o pipefail
 
 if ! command -v cargo-set-version &> /dev/null
 then
@@ -17,62 +19,70 @@ cargo set-version "$NEW_VERSION"
 
 VERSION=v$NEW_VERSION
 
-echo "building version $VERSION"
+echo "Building version $VERSION"
 
-LIN_DIR=m3u-filter_${VERSION}_linux_x86_64
-WIN_DIR=m3u-filter_${VERSION}_windows_x86_64
-DARWIN_DIR=m3u-filter_${VERSION}_darwin_x86_64
-RASPI_DIR=m3u-filter_${VERSION}_armv7_raspi
+declare -A DIRS=(
+    [LINUX]=m3u-filter_${VERSION}_linux_x86_64
+    [WINDOWS]=m3u-filter_${VERSION}_windows_x86_64
+    [RASPI]=m3u-filter_${VERSION}_armv7_raspi
+)
 
-LIN_ARC=${LIN_DIR}.tgz
-WIN_ARC=${WIN_DIR}.zip
-DARWIN_ARC=${DARWIN_DIR}.tgz
-RASPI_ARC=${RASPI_DIR}.tgz
+declare -A ARCS=(
+    [LINUX]=${DIRS[LINUX]}.tgz
+    [WINDOWS]=${DIRS[WINDOWS]}.zip
+    [RASPI]=${DIRS[RASPI]}.tgz
+)
 
-./bin/build_lin_static.sh && \
-# ./bin/build_darwin.sh && \
-./bin/build_raspi.sh && \
-./bin/build_win.sh && \
-./bin/build_fe.sh && \
-cd target && \
-rm -rf "$LIN_DIR" "$RASPI_DIR" "$DARWIN_DIR" "$WIN_DIR" "$LIN_ARC" "$RASPI_ARC" "$DARWIN_ARC" "$WIN_ARC" release_"${VERSION}" && \
-mkdir "$LIN_DIR" && \
-mkdir "$WIN_DIR" && \
-mkdir "$RASPI_DIR" && \
-# mkdir "$DARWIN_DIR" && \
-cp x86_64-unknown-linux-musl/release/m3u-filter "$LIN_DIR" && \
-# cp x86_64-apple-darwin/release/m3u-filter "$DARWIN_DIR" && \
-cp armv7-unknown-linux-musleabihf/release/m3u-filter "$RASPI_DIR" && \
-cp x86_64-pc-windows-gnu/release/m3u-filter.exe "$WIN_DIR" && \
-cp ../config/*.yml "$LIN_DIR" && \
-cp ../config/*.yml "$WIN_DIR" && \
-# cp ../config/*.yml "$DARWIN_DIR" && \
-cp ../config/*.yml "$RASPI_DIR" && \
-cp -rf ../frontend/build "$LIN_DIR"/web && \
-cp -rf ../frontend/build "$WIN_DIR"/web && \
-# cp -rf ../frontend/build "$DARWIN_DIR"/web && \
-cp -rf ../frontend/build "$RASPI_DIR"/web && \
-tar cvzf "$LIN_ARC" "$LIN_DIR" && \
-#  tar cvzf "$DARWIN_ARC" "$DARWIN_DIR" && \
-tar cvzf "$RASPI_ARC" "$RASPI_DIR" && \
-zip -r "$WIN_ARC" "$WIN_DIR" && \
-shasum -a 256 "$LIN_ARC" > checksum.txt && \
-# shasum -a 256 "$DARWIN_ARC" >> checksum.txt && \
-shasum -a 256 "$RASPI_ARC" >> checksum.txt && \
-shasum -a 256 "$WIN_ARC" >> checksum.txt && \
-mkdir "release_${VERSION}" && \
-mv "$LIN_ARC" "release_${VERSION}" && \
-# mv "$DARWIN_ARC" "release_${VERSION}" && \
-mv "$RASPI_ARC" "release_${VERSION}" && \
-mv "$WIN_ARC"  "release_${VERSION}" && \
-mv checksum.txt "release_${VERSION}" && \
-# rm -rf "$LIN_DIR" "$DARWIN_DIR" "$RASPI_DIR" "$WIN_DIR" && \
-rm -rf "$LIN_DIR" "$RASPI_DIR" "$WIN_DIR" && \
-git add . && \
-git commit -m "release ${VERSION}" && \
-git tag -a "$VERSION" -m "$VERSION" && \
-git push && \
+declare -A BINARIES=(
+    [LINUX]=x86_64-unknown-linux-musl/release/m3u-filter
+    [WINDOWS]=x86_64-pc-windows-gnu/release/m3u-filter.exe
+    [RASPI]=armv7-unknown-linux-musleabihf/release/m3u-filter
+)
+
+# Build binaries
+./bin/build_lin_static.sh
+./bin/build_raspi.sh
+./bin/build_win.sh
+./bin/build_fe.sh
+
+cd target
+
+# Clean up previous builds
+rm -rf "${DIRS[@]}" "${ARCS[@]}" release_"${VERSION}"
+
+# Create directories and copy binaries and configuration files
+for PLATFORM in "${!DIRS[@]}"; do
+    DIR=${DIRS[$PLATFORM]}
+    BIN=${BINARIES[$PLATFORM]}
+    ARC=${ARCS[$PLATFORM]}
+
+    mkdir "$DIR"
+    cp "$BIN" "$DIR"
+    cp ../config/*.yml "$DIR"
+    cp -rf ../frontend/build "$DIR"/web
+
+    if [[ $PLATFORM == "WINDOWS" ]]; then
+        zip -r "$ARC" "$DIR"
+    else
+        tar cvzf "$ARC" "$DIR"
+    fi
+
+    shasum -a 256 "$ARC" >> checksum.txt
+done
+
+# Create release directory and move archives
+RELEASE_DIR="release_${VERSION}"
+mkdir "$RELEASE_DIR"
+mv "${ARCS[@]}" checksum.txt "$RELEASE_DIR"
+
+# Clean up build directories
+rm -rf "${DIRS[@]}"
+
+# Commit and tag release
+git add .
+git commit -m "release ${VERSION}"
+git tag -a "$VERSION" -m "$VERSION"
+git push
 git push --tags
-echo "done!"
 
-
+echo "Done!"
