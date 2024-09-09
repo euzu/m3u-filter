266899f2be78675aad52e069c6b8f52475a059ee
euzu <euzu@github.com>
2024-09-09 10:10:40 +0200

    using BPlusTree for persistence

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -4,6 +4,8 @@ pub(crate) mod xtream_repository;
 pub(crate) mod epg_repository;
 pub(crate)mod kodi_repository;
 
+mod bplustree;
+
 mod index_record;
 mod indexed_document_writer;
 mod indexed_document_reader;
