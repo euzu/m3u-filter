ba41e00c4f043d4f32aeb705652f0c9b85f8ae49
jojo141185 <jochen.mittnacht@gmail.com>
2024-12-14 18:52:21 +0100

    Fix: Replace react-tagsinput with lightweight alternative to avoid type problems at compilation

--- frontend/src/component/tag-input/tag-input.tsx
+++ frontend/src/component/tag-input/tag-input.tsx
@@ -1,29 +1,28 @@
-import React, {useCallback, useEffect, useState} from "react";
-import './tag-input.scss';
-import TagsInput from "react-tagsinput";
+import React from "react";
+import { TagsInput } from "react-tag-input-component";
+import "./tag-input.scss";
 
 interface TagInputProps {
-    name: string;
-    values: string[];
-    onChange: (name: string, values: string[]) => void;
+  name: string;
+  values: string[];
+  onChange: (name: string, values: string[]) => void;
 }
 
 export default function TagInput(props: TagInputProps) {
+  const { name, values, onChange } = props;
 
-    const {name, values, onChange} = props;
+  const handleTagsChange = (newTags: string[]) => {
+    onChange(name, newTags);
+  };
 
-    const [tags, setTags] = useState( []);
-
-    useEffect(() =>{
-        if (values) {
-            setTags(values);
-        }
-    }, [values]);
-
-    const handleChange = useCallback((tags: any) => {
-        setTags(tags)
-        onChange(name, tags);
-    }, [name, onChange]);
-
-    return  <TagsInput inputProps={({placeholder: "Add Chat-Id"})} value={tags} onChange={handleChange}></TagsInput>
+  return (
+    <div className="tag-input-container">
+      <TagsInput
+        value={values}
+        onChange={handleTagsChange}
+        name={name}
+        placeHolder="Add tags..."
+      />
+    </div>
+  );
 }
